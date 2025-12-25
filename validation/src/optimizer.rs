fa50dbdbce856bdce0cd024798d65aacf813e22c
Waris <vaxeradev@gmail.com>
2025-12-25 21:51:40 +0530

    Create optimizer.rs

---
+++ validation/src/optimizer.rs
@@ -0,0 +1,27 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_filter_pushdown() {
+        assert!(true);
+    }
+
+    #[test]
+    fn test_projection_pushdown() {
+        assert!(true);
+    }
+
+    #[test]
+    fn test_constant_folding() {
+        assert!(true);
+    }
+
+    #[test]
+    fn test_cost_estimation() {
+        assert!(true);
+    }
+
+    #[test]
+    fn test_join_ordering() {
+        assert!(true);
+    }
+}
