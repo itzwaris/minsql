bed0a82b099745088d281507a697b94be29957da
Waris <vaxeradev@gmail.com>
2025-12-25 20:15:44 +0530

    Create c_api.h

---
+++ bindings/c_api.h
@@ -0,0 +1,14 @@
+#ifndef MINSQL_C_API_H
+#define MINSQL_C_API_H
+
+#include "../storage/include/minsql_storage.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
